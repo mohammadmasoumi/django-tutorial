096a2ed18723e7a7e1f3978e203b3ae5aca435b9
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2022-02-09 00:59:21 +0330

    render html file

--- onlineshop/playground/views.py
+++ onlineshop/playground/views.py
@@ -3,5 +3,9 @@ from django.http import HttpResponse
 
 
 def say_hello(request):
-    
     return HttpResponse("Welcome to the course!")
+
+
+def say_bye(request):
+    context = {'name': 'Mohamamd'}
+    return render(request, 'bye.html', context=context)
