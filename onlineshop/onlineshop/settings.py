372c48b2a401e95ba063b18a655b24c7c409789e
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2022-03-10 05:53:51 +0330

    add sessions app and register models

--- onlineshop/onlineshop/settings.py
+++ onlineshop/onlineshop/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+    'django.contrib.sessions', 
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
--- onlineshop/store/admin.py
+++ onlineshop/store/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from store.models import *
 
 # Register your models here.
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Address)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(Order)
+admin.site.register(OrderItem)
