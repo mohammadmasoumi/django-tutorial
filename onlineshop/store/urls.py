2e02880afeb18776190e912df20cd348564986c7
mohammad masoumi <mohammad.masoomy74@gmail.com>
2022-07-21 08:51:30 +0430

    add product template

--- onlineshop/store/urls.py
+++ onlineshop/store/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('product', product_view),
     # product/{product_id}
+    path('my-product/<slug:slug>/', ProdcutDetailView.as_view(), name='product-detail'),
 ]
--- onlineshop/store/views.py
+++ onlineshop/store/views.py
@@ -1,11 +1,37 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.utils import timezone
 from django.core.exceptions import ObjectDoesNotExist
 from store.models import *
 from tags.models import *
 
-__all__ = ('product_view', )
+__all__ = ('product_view', 'ProdcutDetailView')
 
 
 def product_view(request):
 
     return render(request, 'empty.html')
+
+
+
+class ProdcutDetailView(DetailView):
+    model = Product
+    queryset = Product.objects.all()
+    template_name = "product_template"
+    pk_url_kwarg: str
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
+
+    def get(self, request, *args, **kwargs):
+        """
+        def get(self, request, *args, **kwargs):
+            self.object = self.get_object()
+            context = self.get_context_data(object=self.object)
+            return self.render_to_response(context)
+        """
+
+        return super().get(request, *args, **kwargs)
